e254532144a9192f272b80d46e89a92e459587bf
csuZipple <csu_zipple@qq.com>
2018-05-19 08:51:41 +0800

    init project

---
+++ src/main/java/cn/zippler/xsserver/XsServerApplication.java
@@ -0,0 +1,12 @@
+package cn.zippler.xsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XsServerApplication.class, args);
+	}
+}
