9c0b80abf80aabefad96f1b725ddd8fc99a6ecc9
csuZipple <csu_zipple@qq.com>
2018-05-19 20:04:28 +0800

    reset project

--- src/main/java/cn/zippler/xsserver/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package cn.zippler.xsserver.dao;
-
-import cn.zippler.xsserver.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User,Long> {
-
-}
